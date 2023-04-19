4c7ea79fd9fe5c8a5b479ce9fad5de28bb0da975
jparker1221 <jonparker1221@gmail.com>
2023-04-19 00:18:52 -0400

    added functions to start application and view departments/roles/employees

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Install required packages
+// Install required dependencies
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
@@ -14,3 +14,106 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employees_db database.`)
   );
+
+// Function to start employee tracker application
+function start() {
+  inquirer
+    .prompt({
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "Quit",
+      ]
+    })
+    .then((answer) => {
+      switch(answer.action) {
+        case "View all departments":
+          viewAllDepartments();
+          break;
+        case "View all roles":
+          viewAllRoles();
+          break;
+        case "View all employees":
+          viewAllEmployees();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update an employee role":
+          updateEmployeeRole();
+          break;
+        case "Quit":
+          quit();
+          break;
+      }
+    });
+}
+// Function to view all departments
+function viewAllDepartments() {
+  const query = "SELECT * FROM department";
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    start();
+  });
+}
+
+// Function to view all roles
+function viewAllRoles() {
+  const query = "SELECT * FROM role";
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    start();
+  });
+}
+
+// Function to view all employees
+function viewAllEmployees() {
+  const query = "SELECT * FROM employee";
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    start();
+  });
+}
+
+// Function to add a department
+function addDepartment() {
+
+}
+
+// Function to add a role
+function addRole() {
+
+}
+
+// Function to add an employee
+function addEmployee() {
+
+}
+
+// Function to update an employee role
+function updateEmployeeRole() {
+
+}
+
+// Function to quit application
+function quit() {
+  connection.end();
+  console.log('Goodbye!');
+}
